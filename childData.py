d93f866d443df301f743f5887b9238bc6d2a4a7b
Mina Markos <minalxr123@gmail.com>
2024-07-06 00:57:22 +0300

    database test version

--- childData.py
+++ childData.py
@@ -8,7 +8,7 @@ def AddData():
 
     conn = sqlite3.connect('data.db')
     c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS data (id INTEGER NOT NULL PRIMARY KEY, name TEXT, age FLOAT, parentName TEXT, phone TEXT);')
+    c.execute('CREATE TABLE IF NOT EXISTS data (id INTEGER NOT NULL PRIMARY KEY, name TEXT, age FLOAT, parentName TEXT, phone TEXT, level INTEGER);')
 
     form = st.form(key='new entry')
     with form:
