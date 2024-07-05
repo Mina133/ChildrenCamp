18405236a3e7e14df08bd0c4ec7a407c5f8748c8
Mina Markos <minalxr123@gmail.com>
2024-07-06 00:39:40 +0300

    Your meaningful commit message about changes to data.db

--- DBtests.py
+++ DBtests.py
@@ -2,6 +2,6 @@ import sqlite3
 
 conn = sqlite3.connect('data.db')
 c= conn.cursor()
-c.execute("INSERT INTO Attendence (Cname)  VALUES (?) WHERE Aid = 1 ", ('Mina',))    
+c.execute("DROP TABLE data")    
 conn.commit()
 conn.close()  # Close the database connection
